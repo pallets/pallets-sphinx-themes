34de93120a4ac6c2f5f0c81669022f514defe610
David Lord <davidism@gmail.com>
2018-07-28 14:02:46 -0700

    readthedocs versions

--- pallets_sphinx_themes/__init__.py
+++ pallets_sphinx_themes/__init__.py
@@ -1,65 +1,61 @@
-import inspect
-import os
 import sys
-import textwrap
 from collections import namedtuple
 
+import os
 import pkg_resources
+import textwrap
 
-from pallets_sphinx_themes.versions import DocVersion, VersionManager
+from pallets_sphinx_themes.versions import DocVersion, load_versions
 
-__version__ = '1.0.1'
+__version__ = "1.0.1"
 
 
 def setup(app):
     base = os.path.dirname(__file__)
 
     for name in (
-        'pocoo',
-        'flask',
-        'jinja',
-        'werkzeug',
-        'click',
-        'babel',
-        'platter',
-        'flask-sqlalchemy',
+        "pocoo",
+        "flask",
+        "jinja",
+        "werkzeug",
+        "click",
+        "babel",
+        "platter",
+        "flask-sqlalchemy",
     ):
         app.add_html_theme(name, os.path.join(base, name))
 
-    app.connect('html-page-context', canonical_url)
-    VersionManager(app)
+    app.connect("html-page-context", canonical_url)
+    load_versions(app)
 
-    return {
-        'version': __version__,
-        'parallel_read_safe': True,
-    }
+    return {"version": __version__, "parallel_read_safe": True}
 
 
 def canonical_url(app, pagename, templatename, context, doctree):
-    base = context.get('canonical_url')
+    base = context.get("canonical_url")
 
     if not base:
         return
 
     target = app.builder.get_target_uri(pagename)
-    context['canonical_url'] = base + target
+    context["canonical_url"] = base + target
 
 
 def get_version(name):
     try:
         release = pkg_resources.get_distribution(name).version
     except ImportError:
-        print(textwrap.fill(inspect.cleandoc('''
-            '{name}' must be installed to build the documentation. Install from
-            source using `pip install -e .` in a virtualenv.
-        '''.format(name=name))))
+        print(
+            textwrap.fill(
+                "'{name}' must be installed to build the documentation."
+                " Install from source using `pip install -e .` in a"
+                " virtualenv.".format(name=name)
+            )
+        )
         sys.exit(1)
 
-    if 'dev' in release:
-        release = release.split('dev', 1)[0] + 'dev'
-
-    version = '.'.join(release.split('.', 2)[:2])
+    version = ".".join(release.split(".", 2)[:2])
     return release, version
 
 
-ProjectLink = namedtuple('ProjectLink', ('title', 'url'))
+ProjectLink = namedtuple("ProjectLink", ("title", "url"))
--- pallets_sphinx_themes/versions.py
+++ pallets_sphinx_themes/versions.py
@@ -1,107 +1,127 @@
+from collections import namedtuple
+
 import io
 import json
 import os
-
 from jinja2 import contextfunction
+from packaging import version as pv
 
 
-class VersionManager(object):
-    current = None
-    stable = None
+def load_versions(app):
+    if os.environ.get("READTHEDOCS"):
+        versions = readthedocs_versions(app)
+    else:
+        versions = local_versions(app)
 
-    def __init__(self, app):
-        config_versions = app.config.html_context.get('versions')
-        app.config.html_context['versions'] = self
-        self.versions = versions = []
+    context = app.config.html_context
+    context["versions"] = versions
+    context["current_version"] = next((v for v in versions if v.current), None)
+    context["latest_version"] = next((v for v in versions if v.latest), None)
 
-        if app.config.release.endswith('dev'):
-            current_slug = 'dev'
+
+def local_versions(app):
+    config_versions = app.config.html_context.get("versions")
+
+    if isinstance(config_versions, str):
+        if os.path.isfile(config_versions):
+            with io.open(config_versions, "rt", encoding="utf8") as f:
+                config_versions = json.load(f)
         else:
-            current_slug = app.config.version
+            config_versions = json.loads(config_versions)
 
-        if not config_versions:
-            return
+    if not config_versions:
+        return []
 
-        if isinstance(config_versions, str):
-            if os.path.isfile(config_versions):
-                with io.open(config_versions, 'rt', encoding='utf8') as f:
-                    config_versions = json.load(f)
-            else:
-                config_versions = json.loads(config_versions)
+    versions = []
 
-        for version in config_versions:
-            if isinstance(version, dict):
-                version = DocVersion(**version)
+    for version in config_versions:
+        if isinstance(version, dict):
+            version = DocVersion(**version)
 
-            versions.append(version)
+        versions.append(version)
 
-            if self.current is None and version.slug == current_slug:
-                self.current = version
+    slug = app.config.version
+    dev = 'dev' in app.config.release
+    seen_latest = False
 
-            if self.stable is None and version.note == 'stable':
-                self.stable = version
+    for i, version in enumerate(versions):
+        if version.slug == 'dev':
+            versions[i] = version._replace(dev=True, current=dev)
 
-    @contextfunction
-    def banner(self, context):
-        current = self.current
-        stable = self.stable
+        if version.slug == slug:
+            versions[i] = version._replace(current=True)
 
-        if current is None or stable is None:
-            return
+        if not seen_latest and _is_version(version.slug):
+            seen_latest = True
+            versions[i] = version._replace(latest=True)
 
-        if current.slug == 'dev':
-            return (
-                'This is the development version. The latest stable'
-                ' version is <a href="{href}">{stable}</a>.'
-            ).format(
-                stable=stable.title,
-                href=stable.href(context)
-            )
-
-        elif current.note != 'stable':
-            return (
-                'This is an old version. The latest stable version is'
-                ' <a href="{href}">{stable}</a>.'
-            ).format(
-                stable=stable.title,
-                href=stable.href(context)
-            )
-
-    def __iter__(self):
-        return iter(self.versions)
-
-    def __getitem__(self, item):
-        return self.versions[item]
-
-    def __len__(self):
-        return len(self.versions)
-
-    def __bool__(self):
-        return bool(self.versions)
-
-    __nonzero__ = __bool__
+    return versions
 
 
-class DocVersion(object):
-    def __init__(self, slug, title, note=None, **kwargs):
-        self.slug = slug
-        self.title = title
-        self.note = note
-        self.kwargs = kwargs
+def readthedocs_versions(app):
+    config_versions = app.config.html_context["versions"]
+    current_slug = app.config.html_context["current_version"]
+    latest_slug = None
+
+    for slug, _ in config_versions:
+        if _is_version(slug):
+            latest_slug = slug
+            break
+
+    return [
+        DocVersion(
+            name=slug,
+            slug=slug,
+            latest=slug == latest_slug,
+            dev=slug in {"master", "default", "latest"},
+            current=slug == current_slug,
+        ) for slug, _ in config_versions
+    ]
+
+
+def _is_version(value):
+    try:
+        pv.Version(value)
+        return True
+    except pv.InvalidVersion:
+        return False
+
+
+class DocVersion(namedtuple("DocVersion", ("name", "slug", "latest", "dev", "current"))):
+    __slots__ = ()
+
+    def __new__(cls, name, slug=None, latest=False, dev=False, current=False):
+        slug = slug or name
+        return super(DocVersion, cls).__new__(cls, name, slug, latest, dev, current)
 
     @contextfunction
     def href(self, context):
-        if self == context['versions'].current:
-            return '.'
+        if self.current:
+            return "."
 
-        pathto = context['pathto']
-        master_doc = context['master_doc']
-        pagename = context['pagename']
+        pathto = context["pathto"]
+        master_doc = context["master_doc"]
+        pagename = context["pagename"]
 
         builder = pathto.__closure__[0].cell_contents
-        master = pathto(master_doc).rstrip('#/') or '.'
+        master = pathto(master_doc).rstrip("#/") or "."
         path = builder.get_target_uri(pagename)
-        return '/'.join((master, '..', self.slug, path))
+        return "/".join((master, "..", self.slug, path))
 
-    def __eq__(self, other):
-        return self.slug == other.slug
+    @contextfunction
+    def banner(self, context):
+        if self.latest:
+            return
+
+        latest = context["latest_version"]
+
+        if self.dev:
+            return (
+                "This is the development version. The latest stable"
+                ' version is <a href="{href}">{latest}</a>.'
+            ).format(latest=latest.name, href=latest.href(context))
+
+        return (
+            "This is an old version. The latest stable version is"
+            ' <a href="{href}">{latest}</a>.'
+        ).format(latest=latest.name, href=latest.href(context))
